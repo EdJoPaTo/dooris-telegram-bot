2b2cffbf92dff5025b23167372cce810bf14daa2
Edgar Toll <edjopato@gmail.com>
2018-07-07 20:52:52 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const fs = require('fs')
+const request = require('request-promise-native')
+const Telegraf = require('telegraf')
+
+const { Extra, Markup } = Telegraf
+
+const token = fs.readFileSync(process.env.npm_package_config_tokenpath, 'utf8').trim()
+const bot = new Telegraf(token)
+
+async function doorisStatus() {
+  const response = JSON.parse(await request('https://www.hamburg.ccc.de/dooris/status.json'))
+
+  return response
+}
+
+function statusString(status) {
+  const { open, lastchange } = status.state
+  const age = Date.now() / 1000 - lastchange
+  const ageString = formatAge(age)
+
+  if (open) {
+    return `Die Tür ist seit ${ageString} offen. 🙃`
+  } else {
+    return `Die Tür ist seit ${ageString} geschlossen. 😔`
+  }
+}
+
+function formatAge(ageInSeconds) {
+  if (ageInSeconds < 60 * 90) { // less than 90 min
+    const minutes = Math.floor(ageInSeconds / 60)
+    if (minutes === 1) {
+      return `einer Minute`
+    } else {
+      return `${minutes} Minuten`
+    }
+  } else {
+    const hours = Math.floor(ageInSeconds / (60 * 60))
+    if (hours === 1) {
+      return `einer Stunde`
+    } else {
+      return `${hours} Stunden`
+    }
+  }
+}
+
+bot.command('start', ctx => {
+  return ctx.reply(`Hey ${ctx.from.first_name}!\nBenutze /door für den aktuellen Zustand.\nWenn du Anderen den Zustand der Tür zeigen willst, schreibe in jedem beliebigen Telegram Chat \`@doorisbot\` und wähle den Türzustand. (Die Textzeile darf nichts anderes als \`@doorisbot\` beinhalten)`, Extra.markdown())
+})
+
+bot.command('door', async ctx => ctx.reply(statusString(await doorisStatus())))
+
+bot.command('where', async ctx => {
+  const status = await doorisStatus()
+  const { lon, lat, address } = status.location
+  return ctx.replyWithVenue(lat, lon, 'CCC Hamburg', address)
+})
+
+const updateKeyboard = Markup.inlineKeyboard([
+  Markup.callbackButton('update', 'update')
+])
+
+bot.on('inline_query', async ctx => {
+  const results = []
+
+  const status = await doorisStatus()
+  results.push({
+    type: 'article',
+    id: String(status.state.open),
+    title: `Die Tür ist ${status.state.open ? 'offen' : 'zu'}`,
+    input_message_content: {
+      message_text: statusString(status)
+    },
+    reply_markup: updateKeyboard
+  })
+
+  return ctx.answerInlineQuery(results, {
+    cache_time: 120 // 120 seconds -> 2 minutes
+  })
+})
+
+bot.action('update', async ctx => {
+  const status = await doorisStatus()
+  const text = statusString(status)
+
+  return Promise.all([
+    ctx.editMessageText(text, Extra.markup(updateKeyboard)),
+    ctx.answerCbQuery('updated 😘')
+  ])
+})
+
+bot.on('channel_post', async ctx => {
+  await ctx.reply('Adding a random bot as an admin to your channel is maybe not the best idea…\n\nSincerely, a random bot, added as an admin to this channel.')
+  console.log(`leave the channel…`, ctx.chat)
+  return ctx.leaveChat(ctx.chat.id)
+})
+
+bot.catch(err => {
+  if (err.description === 'Bad Request: message is not modified') return
+  console.error(err)
+})
+
+bot.startPolling()
