26d18d3831ec439d9d86d1dcbde17238d358d9ff
Edgar To <edjopato@gmail.com>
2018-09-03 17:34:14 +0200

    refactor: migrate from eslint to xo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const request = require('request-promise-native')
 const Telegraf = require('telegraf')
 
-const { Extra, Markup } = Telegraf
+const {Extra, Markup} = Telegraf
 
 const tokenFilePath = process.env.NODE_ENV === 'production' ? process.env.npm_package_config_tokenpath : process.env.npm_package_config_tokenpathdebug
 const token = fs.readFileSync(tokenFilePath, 'utf8').trim()
@@ -12,7 +12,7 @@ let statusCache = {}
 let statusTimestamp = 0
 async function doorisStatus() {
   const age = (Date.now() - statusTimestamp) / 1000
-  if (age > 60) { // older than 60 seconds
+  if (age > 60) { // Older than 60 seconds
     statusTimestamp = Date.now()
     statusCache = JSON.parse(await request('https://www.hamburg.ccc.de/dooris/status.json'))
   }
@@ -20,33 +20,29 @@ async function doorisStatus() {
 }
 
 function statusString(status) {
-  const { open, lastchange } = status.state
-  const age = Date.now() / 1000 - lastchange
+  const {open, lastchange} = status.state
+  const age = (Date.now() / 1000) - lastchange
   const ageString = formatAge(age)
 
   if (open) {
     return `Die Tür ist seit ${ageString} offen. 🙃`
-  } else {
-    return `Die Tür ist seit ${ageString} geschlossen. 😔`
   }
+  return `Die Tür ist seit ${ageString} geschlossen. 😔`
 }
 
 function formatAge(ageInSeconds) {
-  if (ageInSeconds < 60 * 90) { // less than 90 min
+  if (ageInSeconds < 60 * 90) { // Less than 90 min
     const minutes = Math.floor(ageInSeconds / 60)
     if (minutes === 1) {
-      return `einer Minute`
-    } else {
-      return `${minutes} Minuten`
+      return 'einer Minute'
     }
-  } else {
-    const hours = Math.floor(ageInSeconds / (60 * 60))
-    if (hours === 1) {
-      return `einer Stunde`
-    } else {
-      return `${hours} Stunden`
-    }
+    return `${minutes} Minuten`
   }
+  const hours = Math.floor(ageInSeconds / (60 * 60))
+  if (hours === 1) {
+    return 'einer Stunde'
+  }
+  return `${hours} Stunden`
 }
 
 bot.command('start', ctx => {
@@ -57,7 +53,7 @@ bot.command('door', async ctx => ctx.reply(statusString(await doorisStatus())))
 
 bot.command('where', async ctx => {
   const status = await doorisStatus()
-  const { lon, lat, address } = status.location
+  const {lon, lat, address} = status.location
   return ctx.replyWithVenue(lat, lon, 'CCC Hamburg', address)
 })
 
@@ -96,13 +92,15 @@ bot.action('update', async ctx => {
 
 bot.on('channel_post', async ctx => {
   await ctx.reply('Adding a random bot as an admin to your channel is maybe not the best idea…\n\nSincerely, a random bot, added as an admin to this channel.')
-  console.log(`leave the channel…`, ctx.chat)
+  console.log('leave the channel…', ctx.chat)
   return ctx.leaveChat(ctx.chat.id)
 })
 
-bot.catch(err => {
-  if (err.description === 'Bad Request: message is not modified') return
-  console.error(err)
+bot.catch(error => {
+  if (error.description === 'Bad Request: message is not modified') {
+    return
+  }
+  console.error(error)
 })
 
 bot.startPolling()
