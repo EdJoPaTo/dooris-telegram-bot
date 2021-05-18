e3c7838953d51f03c9d5bfc83fe995bd555fd4a0
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-05-18 23:32:27 +0200

    feat: migrate to deno and grammY

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,14 @@
-FROM docker.io/library/node:14-alpine AS builder
+FROM docker.io/library/debian:buster AS builder
 WORKDIR /build
 
-COPY package.json package-lock.json tsconfig.json ./
-RUN npm ci
+RUN apt-get update && apt-get install -y curl unzip
+RUN curl -fsSL https://deno.land/x/install/install.sh | sh
+RUN mv /root/.deno/bin/deno /usr/local/bin
 
 COPY source source
-RUN node_modules/.bin/tsc
+RUN /usr/local/bin/deno compile --allow-env --allow-net --output dooris-telegram-bot source/index.ts
 
 
-FROM docker.io/library/node:14-alpine AS packages
-WORKDIR /build
-COPY package.json package-lock.json ./
-RUN npm ci --production
-
-
-FROM docker.io/library/node:14-alpine
-WORKDIR /app
-
-ENV NODE_ENV=production
-
-RUN apk upgrade --no-cache
-
-COPY package.json ./
-COPY --from=packages /build/node_modules ./node_modules
-COPY --from=builder /build/dist ./
-
-CMD node --unhandled-rejections=strict -r source-map-support/register index.js
+FROM docker.io/library/debian:buster-slim
+COPY --from=builder /build/dooris-telegram-bot /usr/local/bin
+CMD /usr/local/bin/dooris-telegram-bot
--- source/index.ts
+++ source/index.ts
@@ -1,148 +1,146 @@
-import {existsSync, readFileSync} from 'fs';
-
-import {InlineQueryResultArticle} from 'typegram';
-import {Telegraf, Markup} from 'telegraf';
-import got from 'got';
+import { Bot, InlineKeyboard } from "https://deno.land/x/grammy@v1.1.4/mod.ts";
+import { InlineQueryResultArticle } from "https://deno.land/x/grammy@v1.1.4/platform.ts";
 
 interface DoorStatusResult {
-	readonly state: {
-		readonly open: boolean;
-		readonly lastchange: number;
-	};
-	readonly location: {
-		readonly lon: number;
-		readonly lat: number;
-		readonly address: string;
-	};
+  readonly state: {
+    readonly open: boolean;
+    readonly lastchange: number;
+  };
+  readonly location: {
+    readonly lon: number;
+    readonly lat: number;
+    readonly address: string;
+  };
 }
 
-process.title = 'dooris-tgbot';
-
-const token = (existsSync('/run/secrets/bot-token.txt') && readFileSync('/run/secrets/bot-token.txt', 'utf8').trim()) ||
-	(existsSync('bot-token.txt') && readFileSync('bot-token.txt', 'utf8').trim()) ||
-	process.env['BOT_TOKEN'];
+const token = Deno.env.get("BOT_TOKEN");
 if (!token) {
-	throw new Error('You have to provide the bot-token from @BotFather via file (bot-token.txt) or environment variable (BOT_TOKEN)');
+  throw new Error(
+    "You have to provide the bot-token from @BotFather via environment variable (BOT_TOKEN)",
+  );
 }
 
-const bot = new Telegraf(token);
+const bot = new Bot(token);
 
 let statusCache: DoorStatusResult;
 let statusTimestamp = 0;
 async function doorisStatus(): Promise<DoorStatusResult> {
-	const age = (Date.now() - statusTimestamp) / 1000;
-	if (age > 60) { // Older than 60 seconds
-		statusTimestamp = Date.now();
-		statusCache = await got('https://www.hamburg.ccc.de/dooris/status.json').json();
-	}
+  const age = (Date.now() - statusTimestamp) / 1000;
+  if (age > 60) { // Older than 60 seconds
+    statusTimestamp = Date.now();
+    const response = await fetch(
+      "https://www.hamburg.ccc.de/dooris/status.json",
+    );
+    statusCache = await response.json();
+  }
 
-	return statusCache;
+  return statusCache;
 }
 
 function statusString(status: DoorStatusResult) {
-	const {open, lastchange} = status.state;
-	const age = (Date.now() / 1000) - lastchange;
-	const ageString = formatAge(age);
+  const { open, lastchange } = status.state;
+  const age = (Date.now() / 1000) - lastchange;
+  const ageString = formatAge(age);
 
-	if (open) {
-		return `Die Tür ist seit ${ageString} offen. 🙃`;
-	}
+  if (open) {
+    return `Die Tür ist seit ${ageString} offen. 🙃`;
+  }
 
-	return `Die Tür ist seit ${ageString} geschlossen. 😔`;
+  return `Die Tür ist seit ${ageString} geschlossen. 😔`;
 }
 
 function formatAge(ageInSeconds: number) {
-	if (ageInSeconds < 60 * 90) { // Less than 90 min
-		const minutes = Math.floor(ageInSeconds / 60);
-		if (minutes === 1) {
-			return 'einer Minute';
-		}
+  if (ageInSeconds < 60 * 90) { // Less than 90 min
+    const minutes = Math.floor(ageInSeconds / 60);
+    if (minutes === 1) {
+      return "einer Minute";
+    }
 
-		return `${minutes} Minuten`;
-	}
+    return `${minutes} Minuten`;
+  }
 
-	const hours = Math.floor(ageInSeconds / (60 * 60));
-	if (hours === 1) {
-		return 'einer Stunde';
-	}
+  const hours = Math.floor(ageInSeconds / (60 * 60));
+  if (hours === 1) {
+    return "einer Stunde";
+  }
 
-	return `${hours} Stunden`;
+  return `${hours} Stunden`;
 }
 
-bot.command('start', async ctx => {
-	return ctx.reply(
-		`Hey ${ctx.from.first_name}!
+bot.command("start", async (ctx) => {
+  return await ctx.reply(
+    `Hey ${ctx.from!.first_name}!
 Benutze /door für den aktuellen Zustand.
-Wenn du Anderen den Zustand der Tür zeigen willst, schreibe in jedem beliebigen Telegram Chat \`@${ctx.botInfo.username}\` und wähle den Türzustand. (Die Textzeile darf nichts anderes als \`@${ctx.botInfo.username}\` beinhalten)`,
-		{parse_mode: 'Markdown'}
-	);
+Wenn du Anderen den Zustand der Tür zeigen willst, schreibe in jedem beliebigen Telegram Chat \`@${username}\` und wähle den Türzustand. (Die Textzeile darf nichts anderes als \`@${username}\` beinhalten)`,
+    { parse_mode: "Markdown" },
+  );
 });
 
-bot.command('door', async ctx => ctx.reply(statusString(await doorisStatus())));
+bot.command(
+  "door",
+  async (ctx) => ctx.reply(statusString(await doorisStatus())),
+);
 
-bot.command('where', async ctx => {
-	const status = await doorisStatus();
-	const {lon, lat, address} = status.location;
-	// TODO: extra is optional... remove when typigns are fixed
-	return ctx.replyWithVenue(lat, lon, 'CCC Hamburg', address, {});
+bot.command("where", async (ctx) => {
+  const status = await doorisStatus();
+  const { lon, lat, address } = status.location;
+  // TODO: extra is optional... remove when typigns are fixed
+  return ctx.replyWithVenue(lat, lon, "CCC Hamburg", address, {});
 });
 
-const updateKeyboard = Markup.inlineKeyboard([
-	Markup.button.callback('update', 'update')
+const updateKeyboard = new InlineKeyboard().text("update", "update");
+
+bot.on("inline_query", async (ctx) => {
+  const results: InlineQueryResultArticle[] = [];
+
+  const status = await doorisStatus();
+  results.push({
+    type: "article",
+    id: String(status.state.open),
+    title: `Die Tür ist ${status.state.open ? "offen" : "zu"}`,
+    input_message_content: {
+      message_text: statusString(status),
+    },
+    reply_markup: updateKeyboard,
+  });
+
+  return ctx.answerInlineQuery(results, {
+    cache_time: 120, // 120 seconds -> 2 minutes
+  });
+});
+
+bot.callbackQuery("update", async (ctx) => {
+  const status = await doorisStatus();
+  const text = statusString(status);
+
+  return Promise.all([
+    ctx.editMessageText(text, { reply_markup: updateKeyboard }),
+    ctx.answerCallbackQuery({ text: "updated 😘" }),
+  ]);
+});
+
+bot.on("channel_post", async (ctx) => {
+  await ctx.reply(
+    "Adding a random bot as an admin to your channel is maybe not the best idea…\n\nSincerely, a random bot, added as an admin to this channel.",
+  );
+  console.log("leave the channel…", ctx.chat);
+  return ctx.leaveChat();
+});
+
+bot.catch((error) => {
+  if (
+    error instanceof Error && error.message.includes("message is not modified")
+  ) {
+    return;
+  }
+
+  console.error(error);
+});
+
+await bot.api.setMyCommands([
+  { command: "door", description: "Zustand der Tür" },
+  { command: "where", description: "Wo ist besagte Tür?" },
 ]);
 
-bot.on('inline_query', async ctx => {
-	const results: InlineQueryResultArticle[] = [];
-
-	const status = await doorisStatus();
-	results.push({
-		type: 'article',
-		id: String(status.state.open),
-		title: `Die Tür ist ${status.state.open ? 'offen' : 'zu'}`,
-		input_message_content: {
-			message_text: statusString(status)
-		},
-		...updateKeyboard
-	});
-
-	return ctx.answerInlineQuery(results, {
-		cache_time: 120 // 120 seconds -> 2 minutes
-	});
-});
-
-bot.action('update', async ctx => {
-	const status = await doorisStatus();
-	const text = statusString(status);
-
-	return Promise.all([
-		ctx.editMessageText(text, updateKeyboard),
-		ctx.answerCbQuery('updated 😘')
-	]);
-});
-
-bot.on('channel_post', async ctx => {
-	await ctx.reply('Adding a random bot as an admin to your channel is maybe not the best idea…\n\nSincerely, a random bot, added as an admin to this channel.');
-	console.log('leave the channel…', ctx.chat);
-	return ctx.leaveChat();
-});
-
-bot.catch(error => {
-	if (error instanceof Error && error.message.includes('message is not modified')) {
-		return;
-	}
-
-	console.error(error);
-});
-
-async function startup() {
-	await bot.telegram.setMyCommands([
-		{command: 'door', description: 'Zustand der Tür'},
-		{command: 'where', description: 'Wo ist besagte Tür?'}
-	]);
-
-	await bot.launch();
-	console.log(new Date(), 'Bot started as', bot.botInfo?.username);
-}
-
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-startup();
+const username = (await bot.api.getMe()).username;
+await bot.start();
